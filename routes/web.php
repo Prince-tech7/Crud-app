0b60b8dc6ff6ee590f97e449c3539226519ff569
Prince-tech <guyprince24@gmail.com>
2025-04-07 18:23:24 +0100

    configuration de url de EtudiantController et importation de la class

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\EtudiantController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/etudiant', [EtudiantController::class, "liste_etudiant"]);
